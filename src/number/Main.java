dfe7eccaded39495e6c6998a43e40c7f0ad76412
chaninkou <michaelkoulove@yahoo.com>
2019-09-09 00:07:17 -0700

    Easy problem, able to find fibonacci number.

--- src/number/Main.java
+++ src/number/Main.java
@@ -1,5 +1,13 @@
 package number;
 
 public class Main {
-
+	public static void main(String[] args) {
+		int N = 3;
+		
+		System.out.println("Input: " + N);
+		
+		FindFibonacciFunction solution = new FindFibonacciFunction();
+		
+		System.out.println("Solution: " + solution.fib(N));
+	}
 }
